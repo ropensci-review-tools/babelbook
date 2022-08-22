0b6b4758d8e69720581585e04f55480dfc0a9fce
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-08-22 16:21:47 +0200

    more

--- orchestration.R
+++ orchestration.R
@@ -5,7 +5,10 @@ temporary_directory <- withr::local_tempdir()
 fs::dir_copy(getwd(), temporary_directory)
 config <- yaml::read_yaml(file.path(temporary_directory, "babelbook", "_quarto.yml"))
 config$lang <- "fr"
+# Here there could be some logic not replacing if there is no .fr equivalent
 config$book$chapters <- gsub("\\.qmd", ".fr.qmd", config$book$chapters)
 yaml::write_yaml(config, file.path(temporary_directory, "babelbook", "_quarto.yml"))
 quarto::quarto_render(file.path(temporary_directory, "babelbook"))
 fs::dir_copy(file.path(temporary_directory, "babelbook", "_book"), file.path("_book", "fr"))
+
+# TODO: add the language switching part
