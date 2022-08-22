6b835eb7554718121ec74f4bd6d07252b2abdf6d
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-08-22 16:06:46 +0200

    more

--- orchestration.R
+++ orchestration.R
@@ -1,3 +1,6 @@
+fs::dir_delete("_book")
+quarto::quarto_render()
+
 temporary_directory <- withr::local_tempdir()
 fs::dir_copy(getwd(), temporary_directory)
 config <- yaml::read_yaml(file.path(temporary_directory, "babelbook", "_quarto.yml"))
@@ -5,3 +8,4 @@ config$lang <- "fr"
 config$book$chapters <- gsub("\\.qmd", ".fr.qmd", config$book$chapters)
 yaml::write_yaml(config, file.path(temporary_directory, "babelbook", "_quarto.yml"))
 quarto::quarto_render(file.path(temporary_directory, "babelbook"))
+fs::dir_copy(file.path(temporary_directory, "babelbook", "_book"), file.path("_book", "fr"))
